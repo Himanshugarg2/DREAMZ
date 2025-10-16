cbd5fd9f95693a304bac7c8fe4323ef53a503c67
Himanshu Garg <himanshu.garg@somaiya.edu>
2025-10-16 23:32:48 +0530

    added code

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '', // ✅ Correct base for Azure Static Web Apps
   build: {
-    outDir: 'build', // ✅ matches Azure workflow (output_location: "build")
+    outDir: 'build',
   },
 })
